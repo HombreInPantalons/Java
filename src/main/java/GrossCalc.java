698a246a41778033b8e35be2c2f3cb514cb9c72b
diazken <dm.anthony1991@gmail.com>
2025-05-23 20:55:07 +0200

    added a gross calculator

---
+++ src/main/java/GrossCalc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class GrossCalc {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Total hours?");
+        int hours = scanner.nextInt();
+        System.out.println("Gross pay?");
+        double pay = scanner.nextDouble();
+
+        double result = pay * hours;
+        System.out.println("total is: "+ result);
+    }
+
+}
